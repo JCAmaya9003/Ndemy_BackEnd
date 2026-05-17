d94778478c9a9cb7df189da4773082ee1441989b
JCAmaya9003 <00365123@uca.edu.sv>
2026-05-17 12:49:50 -0600

    :sparkles: add: course & global handler exception

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/exceptions/AlreadyEnrolledException.java
@@ -0,0 +1,12 @@
+package org.example.ndemy_backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyEnrolledException extends RuntimeException {
+
+    public AlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
