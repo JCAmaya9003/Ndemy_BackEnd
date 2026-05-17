380f711a0e3c966bede1a1d03ea3809999230979
DaniloII09 <00377223@uca.edu.sv>
2026-05-16 19:09:03 -0600

    add: certificate model

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/models/Certificate.java
@@ -0,0 +1,38 @@
+package org.example.ndemy_backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "certificates")
+public class Certificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    /*
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student
+    */
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @CreationTimestamp
+    @Column(name = "issued_at", nullable = false, updatable = false)
+    private LocalDateTime issuedAt;
+
+    @Column(name = "certificate_code", nullable = false, unique = true)
+    // sera un UUID, en la capa de service se parseara a String
+    private String certificationCode;
+}
