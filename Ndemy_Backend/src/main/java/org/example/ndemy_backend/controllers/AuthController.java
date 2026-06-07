ce757042f5d560967800655f118947181a10aa3e
LuisDuque20 <00013423@uca.edu.sv>
2026-06-07 11:54:05 -0600

    feat: Estructura para login con JWT Auth

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package org.example.ndemy_backend.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.ndemy_backend.dto.request.LoginRequest;
+import org.example.ndemy_backend.dto.request.RegisterRequest;
+import org.example.ndemy_backend.dto.response.AuthResponse;
+import org.example.ndemy_backend.services.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.created(null).body(authService.register(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refresh(@RequestHeader("Authorization") String bearerToken) {
+        String token = bearerToken.substring(7);
+        return ResponseEntity.ok(authService.refreshToken(token));
+    }
+}
