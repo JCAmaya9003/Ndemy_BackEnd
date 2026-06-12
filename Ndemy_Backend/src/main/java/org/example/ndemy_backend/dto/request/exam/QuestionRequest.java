b59a426f47dc369636ee63eca812e01654a2f97a
DaniloII09 <00377223@uca.edu.sv>
2026-06-12 15:33:08 -0600

    add: question request

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/dto/request/exam/QuestionRequest.java
@@ -0,0 +1,22 @@
+package org.example.ndemy_backend.dto.request.exam;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionRequest {
+    @NotBlank(message = "Question must have a title")
+    @Size(max = 500, message = "Question title exceeds character limit, max: 200 characters")
+    private String title;
+
+    @PositiveOrZero(message = "Question order index must be 0 or higher")
+    private Integer orderIndex;
+}
