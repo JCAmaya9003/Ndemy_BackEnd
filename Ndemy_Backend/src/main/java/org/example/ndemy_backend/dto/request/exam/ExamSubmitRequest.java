2cb52bc79f2f09da2bd5f7580183c1084ab866d1
DaniloII09 <00377223@uca.edu.sv>
2026-06-12 15:57:08 -0600

    add: exam submit request

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/dto/request/exam/ExamSubmitRequest.java
@@ -0,0 +1,20 @@
+package org.example.ndemy_backend.dto.request.exam;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamSubmitRequest {
+    @NotEmpty(message = "To submit the exam, you must have answered the questions")
+    @Valid
+    private List<AnswerRequest> answers;
+}
