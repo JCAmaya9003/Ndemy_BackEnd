f328d16fd97840501411dfdd9f27374d37705f48
DaniloII09 <00377223@uca.edu.sv>
2026-06-12 15:30:40 -0600

    add: exam request

---
+++ Ndemy_Backend/src/main/java/org/example/ndemy_backend/dto/request/exam/ExamRequest.java
@@ -0,0 +1,24 @@
+package org.example.ndemy_backend.dto.request.exam;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExamRequest {
+    @NotNull(message = "Exam must have a passing score")
+    @Min(value = 60, message = "Exam minimum passing score must be at least 60")
+    @Max(value = 100, message = "Exam maximum passing score is 100")
+    private Integer passingScore;
+
+    @Min(value = 5, message = "Exam minimum time must be 5 minutes")
+    @Max(value = 300, message = "Exam time limit cannot exceed 300 minutes (5 hours)")
+    private Integer timeLimitMinutes;
+}
